dc7831fca32b93d378b81e351f676b41f529665a
João Filippe Rossi Rodrigues <joaorodrigues@eulabs.com.br>
2023-09-18 09:25:17 -0300

    add 1, 2, 3, 4, 5

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello World")
+	fmt.Println("Hello World 1")
 	fmt.Println("Hello World 2")
 	fmt.Println("Hello World 3")
+	fmt.Println("Hello World 4")
+	fmt.Println("Hello World 5")
 }
